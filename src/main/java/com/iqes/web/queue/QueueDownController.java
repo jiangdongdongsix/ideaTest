09650f2014f0ea849aebceead7df9d88612455c1
tamlover <1996hql>
2017-09-05 20:48:00 +0800

    controller

--- src/main/java/com/iqes/web/queue/QueueDownController.java
+++ src/main/java/com/iqes/web/queue/QueueDownController.java
@@ -1,9 +1,28 @@
 package com.iqes.web.queue;
 
+import com.iqes.entity.QueueInfo;
+import com.iqes.service.queue.ExtractNumberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RequestMapping(value = "queue")
 @Controller
+@RequestMapping(value = "queue")
 public class QueueDownController {
+
+    @Autowired
+    private ExtractNumberService extractNumberService;
+
+    @RequestMapping(value = "extractNumber",method = RequestMethod.GET)
+    public QueueInfo extractNumber(@RequestParam("tableName")String tableName){
+        return extractNumberService.ExtractNumber(tableName);
+    }
+
+    @RequestMapping(value = "queryNumber",method = RequestMethod.GET)
+    public QueueInfo queryNumber(){
+
+        return null;
+    }
 }
