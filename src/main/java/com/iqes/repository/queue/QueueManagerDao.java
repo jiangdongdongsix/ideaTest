7a5f6fa4fb958b4fa64ca33999448aa577fd9e15
huqili <543111111@qq.com>
2017-09-06 16:01:33 +0800

    service层的修改

--- src/main/java/com/iqes/repository/queue/QueueManagerDao.java
+++ src/main/java/com/iqes/repository/queue/QueueManagerDao.java
@@ -4,15 +4,19 @@ import com.iqes.entity.QueueInfo;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface QueueManagerDao extends CrudRepository<QueueInfo, Integer> {
 
     //获取同桌类型的排队顾客
-    @Query("select q from QueueInfo q where q.tableType=(select t.tableType from TableNumber t where t.name=?1)")
+    @Query("select q from QueueInfo q where q.tableType=(select t.tableType from TableNumber t where t.name=?1) order by q.id")
     List<QueueInfo> getSameTypeNumbers(String tableName);
 
     //获取有抽号标志的排队顾客
     @Query("select q from QueueInfo q where q.extractFlag='1'")
     List<QueueInfo> getArrivingNumbers();
+
+    //通过id获取queueInfo对象
+    QueueInfo getByIdIs(Long id);
 }
--- src/main/java/com/iqes/service/queue/ExtractNumberService.java
+++ src/main/java/com/iqes/service/queue/ExtractNumberService.java
@@ -1,11 +1,17 @@
 package com.iqes.service.queue;
 
+/**
+ * 抽号service
+ */
+
 import com.iqes.entity.QueueInfo;
+import com.iqes.entity.TableNumber;
 import com.iqes.repository.queue.QueueManagerDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,32 +21,63 @@ public class ExtractNumberService {
     @Autowired
     private QueueManagerDao queueManagerDao;
 
+    //从dao层获取同桌类型的排队顾客
     public List<QueueInfo> getSameTypeNumber(String tName){
         return queueManagerDao.getSameTypeNumbers(tName);
     }
 
+    //排队号的交换
+    public void exchangeNumbers(QueueInfo a){
+
+        QueueInfo b=queueManagerDao.getByIdIs(a.getId()-1);
+        b.setExtractFlag("0");
+        b.setCallCount(0);
+
+        Long id=b.getId();
+        b.setId(a.getId());
+        a.setId(id);
+
+        queueManagerDao.save(a);
+        queueManagerDao.save(b);
+    }
+
     public QueueInfo ExtractNumber(String tableName){
-         List<QueueInfo> queueInfoList=getSameTypeNumber(tableName);
+        //用一个list接
+         List<QueueInfo> queueInfoList= getSameTypeNumber(tableName);
+        TableNumber tableNumber=new TableNumber();
+
          QueueInfo queueInfo=new QueueInfo();
+         QueueInfo qInfo=new QueueInfo();
+
+         //需要交换的标志
+        int ExchanggeFlag=0;
+
          for(QueueInfo q:queueInfoList){
+               if (q.getTableNumber()==tableNumber||q.getTableNumber().equals(null))
+                //判断抽号次数
                 if (q.getExtractCount()<3){
+                    //判断抽号标志
                     if ("0".equals(q.getExtractFlag())){
                         q.setExtractFlag("1");
-                        queueManagerDao.save(q);
+                        q.setExtractCount(q.getExtractCount()+1);
                         queueInfo = q;
+
+                        if (ExchanggeFlag==1){
+                            exchangeNumbers(q);
+                        }else{
+                            queueManagerDao.save(q);
+                        }
                         break;
                     }else {
-                        q.setExtractFlag("0");
-                        continue;
+                        //q.setExtractFlag("0");
+                        //q.setCallCount(0);
+                        ExchanggeFlag=1;
                     }
                 }else {
                     queueManagerDao.delete(q);
                 }
          }
         return queueInfo;
-
     }
-
-
 
 }
