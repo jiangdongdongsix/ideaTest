f49f54792220845161069aea982c9d14d3fd793c
huqili <543111111@qq.com>
2017-09-06 09:07:55 +0800

    dao加了点注释

--- src/main/java/com/iqes/repository/queue/QueueManagerDao.java
+++ src/main/java/com/iqes/repository/queue/QueueManagerDao.java
@@ -8,9 +8,11 @@ import java.util.List;
 
 public interface QueueManagerDao extends CrudRepository<QueueInfo, Integer> {
 
+    //获取同桌类型的排队顾客
     @Query("select q from QueueInfo q where q.tableType=(select t.tableType from TableNumber t where t.name=?1)")
     List<QueueInfo> getSameTypeNumbers(String tableName);
 
+    //获取有抽号标志的排队顾客
     @Query("select q from QueueInfo q where q.extractFlag='1'")
     List<QueueInfo> getArrivingNumbers();
 }
