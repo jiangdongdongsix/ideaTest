d62dc0e892d0ec36430ffa688ac4c672c594dfcd
LongSheLA <475992348@qq.com>
2017-09-07 09:25:59 +0800

    “排队类添加字段”

--- src/main/java/com/iqes/repository/TableTypeDao.java
+++ src/main/java/com/iqes/repository/TableTypeDao.java
@@ -9,15 +9,12 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
 
-public interface TableDao extends PagingAndSortingRepository<TableType, Long>, JpaSpecificationExecutor<TableType> {
+public interface TableTypeDao extends PagingAndSortingRepository<TableType, Long>, JpaSpecificationExecutor<TableType> {
 
-      @Modifying
       @Query(value = "SELECT * FROM table_type as ty where ty.eat_min_number<?1 and ?1<=ty.eat_max_number",nativeQuery = true)
       List<TableType> getTableType(Integer eatNum);
 
-      @Query(value = "SELECT count(id) FROM table_number where table_type_id =?1;",nativeQuery = true)
+      @Query(value = "SELECT count(*) FROM table_number as tn where tn.table_type_id = ?1",nativeQuery = true)
       Integer getTableCountByType(long tableTypeId);
-
-
 
 }
