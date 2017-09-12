7d7970ec8ef2778ede8fdae1a40d0101c8603127
huqili <543111111@qq.com>
2017-09-12 10:52:17 +0800

    配置文件dao层

---
+++ src/main/java/com/iqes/repository/restaurant/ConfigInfoDao.java
@@ -0,0 +1,7 @@
+package com.iqes.repository.restaurant;
+
+import com.iqes.entity.ConfigInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConfigInfoDao extends CrudRepository<ConfigInfo,Long> {
+}
