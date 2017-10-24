032b2d890bd988b20c83c5389998fca4d08a5986
huqili <543111111@qq.com>
2017-10-24 09:46:27 +0800

    门店基本信息修改

--- src/main/java/com/iqes/entity/RestaurantInfo.java
+++ src/main/java/com/iqes/entity/RestaurantInfo.java
@@ -15,6 +15,15 @@ public class RestaurantInfo extends IdEntity {
     private String detailAddress;
     private String openTime;
     private String endTime;
+    private String telephone;
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public String getName() {
         return name;
